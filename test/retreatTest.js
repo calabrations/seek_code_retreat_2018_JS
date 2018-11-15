5b2d73edf96f1cf75459d46aae715189e9bd2efd
Michael <mcalabrese.dev@gmail.com>
2018-11-15 22:43:40 +1100

    initial check test pass ok with Mocha

---
+++ test/retreatTest.js
@@ -0,0 +1,9 @@
+var add = require('../src/retreat');
+var assert = require('assert');
+
+describe('retreat', function () {
+    it('should return hello', function () {
+        assert.equal(add(1,1), 2)
+    });
+});
+
